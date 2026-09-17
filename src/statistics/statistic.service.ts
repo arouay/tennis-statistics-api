709b8205851de90cb766d4a1a158c57e773d872e
arouay <arouayabdelalim@gmail.com>
2026-09-18 00:13:43 +0200

    Chore | added more implementation context

--- src/statistics/statistic.service.ts
+++ src/statistics/statistic.service.ts
@@ -8,6 +8,7 @@ export class StatisticService {
   async getSummary(): Promise<StatisticsSummary> {
     const playerStats = await this.statisticRepository.findAllPlayerStats();
 
+    // Note: Calcul en TypeScript (possible en SQL) pour rester testable unitairement sans DB.
     return {
       topCountryByWinRatio: findTopCountryByWinRatio(playerStats),
       averageBmi: calculateAverageBmi(playerStats),
